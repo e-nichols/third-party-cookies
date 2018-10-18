33a21745b9e8fa4d39d67f49ce82e1870c446e31
Evan Nichols <e-nichols@users.noreply.github.com>
2018-10-18 15:45:32 -0400

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -5,14 +5,13 @@ function are_cookies_enabled()
 	var isNavCookieEnabledUndefined = typeof navigator.cookieEnabled === "undefined";
 	console.log('cookieEnabled value: ' + cookieEnabled);
 	console.log('isNavCookieEnabled undefined? -> ' + isNavCookieEnabledUndefined);
-
-	if (isNavCookieEnabledUndefined || !cookieEnabled)
-	{ 
-		console.log('nav cookie undefined and cookie disabled... trying to write garbage cookie.');
-		document.cookie="testcookie";
-		cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
-		console.log('cookieEnabled value after write: ' + cookieEnabled);
-	}
-        var status = cookieEnabled ? 'enabled' : 'disabled';
+	
+	console.log('Attempting to write garbage cookie.');
+	document.cookie="testcookie";
+	cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+	
+	console.log('cookieEnabled value after write: ' + cookieEnabled);
+        
+	var status = cookieEnabled ? 'enabled' : 'disabled';
         alert('Third party cookies are ' + status + '.');
 }
